bb8fe023f67a0037d2535db27719338c52594c12
Julian Haupt <julian.haupt@hauptmedia.de>
2018-11-13 19:38:05 +0100

    sunxi: always do full system upgrade and preserve u-boot overlay environment

--- target/linux/sunxi/base-files/lib/upgrade/platform.sh
+++ target/linux/sunxi/base-files/lib/upgrade/platform.sh
@@ -2,30 +2,7 @@ REQUIRE_IMAGE_METADATA=1
 REQUIRE_IMAGE_SIGNATURE=1
 
 platform_check_image() {
-	local diskdev partdev diff
-
-	export_bootdevice && export_partdevice diskdev -2 || {
-		echo "Unable to determine upgrade device"
-		return 1
-	}
-
-	get_partitions "/dev/$diskdev" bootdisk
-
-	#extract the boot sector from the image
-	get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b 2>/dev/null
-
-	get_partitions /tmp/image.bs image
-
-	#compare tables
-	diff="$(grep -F -x -v -f /tmp/partmap.bootdisk /tmp/partmap.image)"
-
-	rm -f /tmp/image.bs /tmp/partmap.bootdisk /tmp/partmap.image
-
-	if [ -n "$diff" ]; then
-		echo "Partition layout has changed. Full image will be written."
-		ask_bool 0 "Abort" && exit 1
-		return 0
-	fi
+	return 0;
 }
 
 platform_copy_config() {
@@ -48,45 +25,17 @@ platform_do_upgrade() {
 
 	sync
 
-	if [ "$SAVE_PARTITIONS" = "1" ]; then
-		get_partitions "/dev/$diskdev" bootdisk
+	dd if="/dev/$diskdev" of=/tmp/u-boot.env bs=1024 skip=544 count=128
 
-		#extract the boot sector from the image
-		get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b
+	get_image "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
 
-		get_partitions /tmp/image.bs image
+	dd if=/tmp/u-boot.env of="/dev/$diskdev" bs=1024 seek=544 conv=fsync
+	rm /tmp/u-boot.env
 
-		#compare tables
-		diff="$(grep -F -x -v -f /tmp/partmap.bootdisk /tmp/partmap.image)"
-	else
-		diff=1
-	fi
+	# Separate removal and addtion is necessary; otherwise, partition 1
+	# will be missing if it overlaps with the old partition 2
+	partx -d - "/dev/$diskdev"
+	partx -a - "/dev/$diskdev"
 
-	if [ -n "$diff" ]; then
-		get_image "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
-
-		# Separate removal and addtion is necessary; otherwise, partition 1
-		# will be missing if it overlaps with the old partition 2
-		partx -d - "/dev/$diskdev"
-		partx -a - "/dev/$diskdev"
-
-		return 0
-	fi
-
-	#write uboot image
-	get_image "$@" | dd of="$diskdev" bs=1024 skip=8 seek=8 count=1016 conv=fsync
-	#iterate over each partition from the image and write it to the boot disk
-	while read part start size; do
-		part="$(($part - 2))"
-		if export_partdevice partdev $part; then
-			echo "Writing image to /dev/$partdev..."
-			get_image "$@" | dd of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
-		else
-			echo "Unable to find partition $part device, skipped."
-		fi
-	done < /tmp/partmap.image
-
-	#copy partition uuid
-	echo "Writing new UUID to /dev/$diskdev..."
-	get_image "$@" | dd of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
+	return 0
 }
